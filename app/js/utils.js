9327568fac39a9e8fdcfe3d78cb797d551d32b5f
Lauren Ko <kolauren@gmail.com>
2013-10-25 20:49:26 -0700

    Added the most of the js for creating a static dependency wheel

---
+++ app/js/utils.js
@@ -0,0 +1,27 @@
+/**
+ * Singleton class for all utils
+ *
+ */
+c.Utils = (function() {
+
+    var instance;
+
+    function init() {
+        return {
+            /**
+             * @param hue, saturation, lightness
+             * @return a stringified random hsl value
+             **/
+            getColour: function(h, s, l) {
+                return "hsl(" + h + "," + s + "%," + l + "%)";
+            }
+        };
+    }
+
+    return {
+        getInstance: function() {
+            if(!instance) instance = init();
+            return instance;
+        }
+    }
+})();
