2500cef65c0ee232cadbfa50dabc3b225b1e40ab
Lauren Ko <kolauren@gmail.com>
2013-11-06 23:27:23 -0800

    fixed up the animation stuff... sorta working now

--- app/js/animation.js
+++ app/js/animation.js
@@ -17,129 +17,47 @@ Animation.prototype = {
         json: "",
         startButton: ""
     },
-
-    currentNodes: [],
-    currentDependencies: [],
-
-    // will be refreshed every commit for animations
-    addedJavaFiles: [],
-    modifiedJavaFiles: [],
-    removedJavaFiles: [],
-    renamedJavaFiles: [],
-    dependenciesAffected: [],
     commits: [],
     interval: 1500,
     timer: null,
     dependencyWheel: null,
+    frame: 0,
 
     init: function() {
-        this.options.dependencyWheel = new DependencyWheel({ selector: this.options.selector, 
-            json: this.options.json });
+        this.dependencyWheel = new DependencyWheel({ selector: this.options.selector});
         var self = this;
         
+        this.utils.processCommitData(function(commits){
+            self.commits = commits;
+            var currentEdges = self.commits[self.commits.length - 1].currentEdges;
+            var currentNodes = self.commits[self.commits.length - 1].currentNodes;
+            self.dependencyWheel.draw(currentNodes, currentEdges);
+        });
+
         $(this.options.startButton).click(function(){
-            // process the commit data
-            self.processCommitData();
             self.startAnimation();
             $(self.options.startButton).attr("disabled", true);
         });
     },
-    
-    
-    dependencyExists: function(dependencyList, dependency) {
-        exists = false;
-        dependencyList.forEach(function(d) {
-            if(d.source === dependency.source && d.target === dependency.target)
-                exists = true;
-        });
-        return exists;
-    },
-    
-    
-    // TODO: REFACTOR!!
-    processCommitData: function() {
-        // new fields for pushing current states for each commit
-        var self = this;
-        var nodes = [];
-        var edges = [];
-        
-        $.getJSON("commitsTest2.json", function(resp) {
-            self.commits = resp;
-            self.commits.forEach(function(c){
-                // loop through added files
-                // add added files to nodes array
-                c.addedJavaFiles.forEach(function(a) {
-                  nodes = nodes.concat({"name": a, "deleted": false});  
-                    
-                });
-                //nodes = nodes.concat(c.addedJavaFiles);
-                
-                  // loop through dependencies
-                // add dependency if it doesnt exist already in edges array
-                c.dependencies.forEach(function (d){
-                    if(!self.dependencyExists(edges, d))
-                        //edges.push(d);   <====== fffffffffffffffffff youuuu!!!!!!
-                        edges = edges.concat(d);
-                });
-                
-                
-                // loop through renamed files, rename files in nodes array
-                c.renamedJavaFiles.forEach(function(r){
-                    // search the current nodes list and replace the old with new name
-                    var i = 0;
-                    for(i; i<nodes.length; i++) {
-                     if(nodes[i] === r.old) {
-                         nodes[i] = r.new;
-                         break;
-                     }
-                    }
-                    
-                    // rename the edge if it contains renamed node??
-                    edges.forEach(function(e) {
-                        // if the edge contains the node, rename it??
-                        if(e.target === r.old)
-                            e.target = r.new;
-                        else if(e.source === r.old)
-                            e.source = r.new;
-                    });
-                });
-                
-                // loop through removed files, set deleted flag in nodes array to true
-                c.removedJavaFiles.forEach(function(d) {
-                    var j = 0;
-                    for(j; j<nodes.length; j++) {
-                     if(nodes[j].name === d) {
-                         nodes[j].deleted = "true";
-                         continue;
-                     }
-                    }
-                // set deleted flag to true for all associated edges
-                    edges.forEach(function(e) {
-                        // if the edge contains the node, set deleted flag = true
-                        if(e.target === d || e.source === d)
-                            e.deleted = "true";
-                    });
-
-                });
-                
-                // set commit object's currentNode and currentEdges fields
-                c.currentNodes = nodes;
-                c.currentEdges = edges;
-            });
-            
-        });
-        
-    },
 
     startAnimation: function() {
         var self = this;
-        self.clearWheel();
-        $.getJSON("commitTest1.json", function(resp) {
-            self.commits = resp.reverse();
+        self.frame = 0;
+        self.timer = setTimeout(function(){
+            self.animationCallback();
+        }, self.interval);
+    },
+
+    // animate commits in different intervals
+    animationCallback: function() {
+        var self = this;
+        self.animateCommits();
+        self.frame++;
+        clearTimeout(self.timer);
+        if(self.frame < self.commits.length) 
             self.timer = setTimeout(function(){
                 self.animationCallback();
             }, self.interval);
-        });
     },
 
     pauseAnimation: function() {
@@ -150,108 +68,21 @@ Animation.prototype = {
     jumpTo: function(frame) {
     },
 
-    // animate commits in different intervals
-    animationCallback: function() {
-        var self = this;
-        console.log("callback " + this.commits.length);
-        self.animateCommits();
-        clearTimeout(self.timer);
-        if(this.commits.length > 0) 
-            self.timer = setTimeout(function(){
-                self.animationCallback();
-            }, self.interval);
-    },
-
     // clears the canvas of nodes and edges
     clearWheel: function() {
         d3.selectAll("circle").remove();
         d3.selectAll("path.edge").remove();
     },
 
-    // returns a list of dependencies of given class
-    getDependencies: function(className){
-        var dependencies = [];
-        this.currentDependencies.forEach(function (d){
-           if(d[0] === className)
-               dependencies.push(d[1]);
-        });
-        return dependencies;
-    },
-
-    // creates the tree for parsing using the d3 cluster method
-    createTree:  function(currentNodes) {
-        var root = {name: "root", children: []};
-        var self = this;
-        currentNodes.forEach(function(n){
-            var child = {name: n, imports: self.getDependencies(n)};
-            root.children.push(child);
-        });
-        return root;
-    },
-
     // redraws the dependency graph
     redraw: function () {
-        this.clearWheel();
-        var tree = this.createTree(this.currentNodes);
-        this.dependencyWheel.draw(tree);
-    },
-
-    // checks if the dependency is already added
-    dependenciesAlreadyAdded: function (source, destination) {
-        var i = 0;
-        for (i; i < this.currentDependencies.length; i++) {
-            if (this.currentDependencies[i][0] === source) {
-                if (this.currentDependencies[i][1] === destination) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    },
-
-    // updates current node and connections 
-    // reads a commit and adds new nodes, removes nodes, 
-    update: function (commit) {
-        var self = this;
-        this.currentNodes = this.currentNodes.concat(commit.addedJavaFiles);
-        var i = 0;
-        for (i; i < commit.dependenciesAffected.length; i++) {
-            if (!this.dependenciesAlreadyAdded(commit.dependenciesAffected[i][0],commit.dependenciesAffected[i][1])) {
-                this.currentDependencies.push(commit.dependenciesAffected[i]);
-            }
-        }
-        
-        // remove the node from current node list
-        commit.removedJavaFiles.forEach(function(deleted){
-            self.currentNodes = self.currentNodes.filter(function(elem){
-                return !(elem === deleted)
-            });
-        });
-        
-        // remove the dependencies of the removed classes
-        commit.removedJavaFiles.forEach(function(deleted){
-            self.currentDependencies = self.currentDependencies.filter(function(elem){
-                return !((elem[0] === deleted) || (elem[1] === deleted))
-            });
-        });
-        
-        // flush out the old
-        this.addedJavaFiles = [];
-        this.modifiedJavaFiles = [];
-        this.removedJavaFiles = [];
-        this.renamedJavaFiles = [];
-        // add the current data for current commit to be animated
-        
-        this.addedJavaFiles = this.addedJavaFiles.concat(commit.addedJavaFiles);
-        this.modifiedJavaFiles = this.modifiedJavaFiles.concat(commit.modifiedJavaFiles);
-        this.removedJavaFiles = this.removedJavaFiles.concat(commit.removedJavaFiles);
-        this.renamedJavaFiles = this.renamedJavaFiles.concat(commit.renamedJavaFiles);
+        //this.dependencyWheel.draw(tree);
     },
 
     animateCommits: function () {
-        var commit = this.commits.pop();
-        this.update(commit);
-        this.redraw();
+        var commit = this.commits[this.frame];
+        console.log(commit);
+        this.dependencyWheel.lightUp(commit.addedJavaFiles.concat(commit.modifiedJavaFiles), commit.removedJavaFiles, commit.dependencies);
     },
     
     // TODO: creates a static wheel for the beginning of animation
--- app/js/dependencywheel.js
+++ app/js/dependencywheel.js
@@ -14,7 +14,6 @@ DependencyWheel.prototype = {
   cluster: null,
 
   options: {
-    json: "",
     selector: "", /* class or id of the element containing the dependency wheel */
     radius: 300,
     line: null, /* line function */
@@ -27,7 +26,7 @@ DependencyWheel.prototype = {
     this.svg = d3.select(this.options.selector)
       .append("svg:svg")
       .attr("width", (this.options.radius * 2))
-      .attr("height", (this.options.radius * 2))
+      .attr("height", (this.options.radius * 2.1))
       .append("svg:g")
       .attr("transform", "translate(" + (this.options.radius) + "," + (this.options.radius) + ")");
 
@@ -51,68 +50,113 @@ DependencyWheel.prototype = {
   },
 
   // Draws all the nodes and edges based on input data
-  draw: function(data) {
+  draw: function(currentNodes, currentEdges) {
       var self = this;  
-      var nodes = self.getNodes(data);
-      var edges = self.getEdges(nodes);
-      var splines = self.options.bundle(edges);
+      var d3data = self.parseDataToD3(currentNodes, currentEdges);
+      var splines = self.options.bundle(d3data.edges);
 
       var paths = self.svg.selectAll(self.options.selector)
-        .data(edges).enter().append("svg:path")
-        .attr("class", function(d) { return "edge source-" + d.source.name + " target-" + d.target.name; })
+        .data(d3data.edges).enter().append("svg:path")
+        .attr("class", function(d) { return d.class; })
         .style("stroke", function(d) { 
-          return self.utils.getColour(d.source.hue, 50, 80); })
+          return self.utils.getColour(d.source.hue, 70, 60); })
+        .style("opacity", 0.2)
         .attr("d", function(d, i) { return self.options.line(splines[i]); });
 
       self.svg.selectAll("g")
-        .data(nodes.filter(function(n) { return !n.children; }))
+        .data(d3data.nodes.filter(function(n) { return !n.children; }))
         .enter().append("circle")
         .attr("r", 10)
-        .attr("class", function(d) { return "node " + d.name; })
-        .style("fill", function(d) { return self.utils.getColour(d.hue, 50, 60); })
+        .attr("class", function(d) { return "node " + d.id; })
+        .style("fill", function(d) { return self.utils.getColour(d.hue, 70, 60); })
+        .style("opacity", 0.2)
         .attr("data-name", function(d) { return d.name; })
         .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
         .append("svg:title")
         .text(function(d) { return d.name; });   
-  }
-  
-  // Move these json extraction methods to utils
-  // /** 
-  //  * @param json data
-  //  * @return returns an array of d3.cluster nodes
-  //  **/
-  // getNodes: function(data) {
-  //   var self = this;
-  //   var nodes = self.cluster.nodes(data);
-  //   // pick hues for each node that are equidistant apart from one another
-  //   var colours = []
-  //   var increment = Math.ceil(360 / nodes.length);
-  //   for(var i = 0; i < 360 && colours.length <= nodes.length; i += increment) {
-  //     colours.push(i);
-  //   }
-  //   nodes.forEach(function(n){
-  //     n.hue = colours.pop();
-  //   });
-  //   return nodes;
-  // },
+  },
 
-  // /** 
-  //  * @param nodes The nodes created from getNodes()
-  //  * @return an array of edges
-  //  **/
-  // getEdges: function(nodes) {
-  //   var map = {};
-  //   var edges = [];
-  //   nodes.forEach(function(d) {
-  //     map[d.name]  = d;
-  //   });
-  //   nodes.forEach(function(d) {
-  //     if(d.imports) {
-  //       d.imports.forEach(function(a) {
-  //         edges.push({source: map[d.name], target: map[a]});
-  //       });
-  //     }
-  //   });
-  //   return edges;
-  // }
+  lightUp: function(modified, removed, dependencies) {
+    var self = this;
+    modified.forEach(function(n){
+      var selector = ".node." + n;
+      console.log(selector);
+      self.svg.select(selector)
+        .transition()
+        .delay(function(d,i) { return i * 10; })
+        .duration(1250)
+        .style('opacity', 1);
+    });
+
+    removed.forEach(function(n) {
+      self.svg.select(n).remove();
+    });
+
+    dependencies.forEach(function(n) {
+      console.log(".edge.source-" + n.source + ".target-" + n.target);
+      self.svg.select(".edge.source-" + n.source + ".target-" + n.target)
+        .transition()
+        .delay(function(d,i) { return i * 10; })
+        .duration(1250)
+        .style('opacity', 1);
+    });
+
+    d3.selectAll("path.edge").style("opacity", 0.2);
+    d3.selectAll("circle.node").style("opacity", 0.2);
+  },
+
+  // parses the data so it will be appropriate to pass to D3
+  parseDataToD3:  function(currentNodes, currentEdges) {
+      var self = this;
+      var colours = [];
+      var edges = [];
+      var cluster_map = {};
+
+      // creating equidistant color math
+      var increment = Math.ceil(360 / currentNodes.length);
+      for(var i = 0; i < 360 && colours.length <= currentNodes.length; i += increment) {
+        colours.push(i);
+      }
+
+      // adding a colour item to each node
+      currentNodes.forEach(function(n) {
+          if(n.oldName)
+            n.id = n.oldName + " " + n.name;
+          else n.id = n.name;
+          n.hue = colours.pop();
+      });
+
+      // create the nodes from cluster
+      var nodes = self.cluster.nodes({name: "root", children: currentNodes});
+      // create a map of each node for easy reference
+      nodes.forEach(function(d) {
+        cluster_map[d.name]  = d;
+      });
+
+      // create edges from edge data
+      currentEdges.forEach(function(n) {
+        if(cluster_map[n.source] && cluster_map[n.target]) {
+          var css_class = "edge source-" + cluster_map[n.source].name + " target-" + cluster_map[n.target].name;
+          if(cluster_map[n.source].oldName) css_class += " source-" + cluster_map[n.source].oldName;
+          if(cluster_map[n.target].oldName) css_class += " target-" + cluster_map[n.target].oldName;
+          edges.push({ source: cluster_map[n.source], target: cluster_map[n.target], "class": css_class });
+        }
+      });
+
+      return {
+        nodes: nodes,
+        edges: edges
+      };
+  },
+
+  // returns a list of dependencies of given class
+  // helper function for parsing data
+  getDependencies: function(className, currentEdges){
+      var dependencies = [];
+      currentEdges.forEach(function (d){
+         if(d.target === className)
+             dependencies.push(d.source);
+      });
+      return dependencies;
+  }
 };
